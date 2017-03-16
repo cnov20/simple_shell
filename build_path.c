6f0dc81efca749863fdce1d22cd813578d524ac1
cnov20 <128@holbertonschool.com>
2017-03-16 22:48:22 +0000

    Added two files to handle path so single commands can be executed

---
+++ build_path.c
@@ -0,0 +1,23 @@
+#include "shell.h"
+
+char *build_path(char *token, char *value)
+{
+	char *command_path;
+	size_t command_len;
+
+	command_len = _strlen(value) + _strlen(token) + 2;
+	command_path = malloc(sizeof(command_len));
+
+	if (command_path != NULL)
+	{
+		return (NULL);
+	}
+
+	_memset(command_path, 0, command_len);
+
+	command_path = _strcat(command_path, value);
+	command_path = _strcat(command_path, "/");
+	command_path = _strcat(command_path, token);
+
+	return (command_path);
+}
