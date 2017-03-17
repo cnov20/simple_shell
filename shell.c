5960d16bed95356c4779c71a2c067e31e17e8d90
cnov20 <128@holbertonschool.com>
2017-03-17 10:06:41 +0000

    Added functionality to handle exit() built-in with arguments - Added appropriate free() calls

--- shell.c
+++ shell.c
@@ -6,26 +6,31 @@
  * Return: int - indicating success or failure
  */
 
-int main (void)
+int main(void)
 {
 	char **argv;
 	char *line;
+	int status = 0;
 
 	_putstring(PROMPT);
-
 	while (1)
 	{
 		line = read_line();
-
 		argv = tokenizer(line);
 
 		if (_strcmp(line, "exit") == 0)
 		{
+			if (argv[1])
+			{
+				status = _atoi(argv[1]);
+				free(argv);
+				free(line);
+				exit(status);
+			}
 			free(line);
 			free(argv);
-		        exit(EXIT_SUCCESS);
+			exit(EXIT_SUCCESS);
 		}
-
 		if (_strcmp(line, "env") == 0)
 		{
 			_print_env();
@@ -34,10 +39,8 @@ int main (void)
 		execute_cmd(argv, line);
 
 		_putstring(PROMPT);
-
 		free(line);
 		free(argv);
-
 	}
 
 	return (0);
